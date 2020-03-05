5fa972e98709102259075dd600e9f809fb321782
Mikhail7788 <mikhail843@gmail.com>
2020-03-05 20:28:41 +0300

    api/MoneyManager correct

--- public/api/MoneyManager.js
+++ public/api/MoneyManager.js
@@ -42,7 +42,7 @@ class MoneyManager {
     }
 
     sendMoneyAction(){
-        const amount = this.sendMoneyForm.querySelector('[placeholder="Сумма"]').value;
+        const amount += this.sendMoneyForm.querySelector('[placeholder="Сумма"]').value;
         const to = this.sendMoneyForm.getElementsByTagName('select')[0].value;
         const currency = this.sendMoneyForm.getElementsByTagName('select')[1].value;
         this.sendMoneyCallback({to, amount, currency});
